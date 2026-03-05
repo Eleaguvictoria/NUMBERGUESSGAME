53bb4d9c37fee6eb7a865a11d44d6b9c49c4bfbd
Victoria <Victoriaeleagu@gmail.com>
2026-03-05 18:33:33 +0200

    Fix class declaration in NumberGuessTest

--- src/test/java/com/studentapp/NumberGuessTest.java
+++ src/test/java/com/studentapp/NumberGuessTest.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-public class NumberGuessServletTest.java {
+public class NumberGuessServletTest {
     private NumberGuessServlet servlet;
     private HttpServletRequest request;
     private HttpServletResponse response;
