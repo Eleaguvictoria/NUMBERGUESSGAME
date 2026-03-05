2b09689e9704938edd967464bbe09ef5744c92ac
Victoria <Victoriaeleagu@gmail.com>
2026-03-05 18:46:43 +0200

    Refactor NumberGuessServlet and add game logic

--- src/test/java/com/studentapp/NumberGuessTest.java
+++ src/test/java/com/studentapp/NumberGuessTest.java
@@ -1,49 +1,53 @@
 package com.studentapp;
 
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 
-public class NumberGuessServletTest {
-    private NumberGuessServlet servlet;
-    private HttpServletRequest request;
-    private HttpServletResponse response;
-    private StringWriter responseWriter;
+public class NumberGuessServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private int targetNumber;
 
-    @Before
-    public void setUp() throws Exception {
-        servlet = new NumberGuessServlet();
-        servlet.init();
-        request = Mockito.mock(HttpServletRequest.class);
-        response = Mockito.mock(HttpServletResponse.class);
-        responseWriter = new StringWriter();
-        Mockito.when(response.getWriter()).thenReturn(new PrintWriter(responseWriter));
+    public void init() throws ServletException {
+        targetNumber = new Random().nextInt(100) + 1;
     }
 
-    @Test
-    public void testGuessTooLow() throws Exception {
-        Mockito.when(request.getParameter("guess")).thenReturn("1");
-        servlet.doPost(request, response);
-        assertTrue(responseWriter.toString().contains("Your guess is too low"));
+    public int getTargetNumber() {
+        return targetNumber;
     }
 
-    @Test
-    public void testGuessTooHigh() throws Exception {
-        Mockito.when(request.getParameter("guess")).thenReturn("100");
-        servlet.doPost(request, response);
-        assertTrue(responseWriter.toString().contains("Your guess is too high"));
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Number Guessing Game</h1>");
+        out.println("<form action='guess' method='post'>");
+        out.println("Guess a number between 1 and 100: <input type='text' name='guess' />");
+        out.println("<input type='submit' value='Submit' />");
+        out.println("</form>");
     }
 
-    @Test
-    public void testCorrectGuess() throws Exception {
-        int correctGuess = servlet.getTargetNumber();
-        Mockito.when(request.getParameter("guess")).thenReturn(String.valueOf(correctGuess));
-        servlet.doPost(request, response);
-        assertTrue(responseWriter.toString().contains("Congratulations! You guessed the number!"));
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        try {
+            int guess = Integer.parseInt(request.getParameter("guess"));
+            if (guess < targetNumber) {
+                out.println("<h2>Your guess is too low. Try again!</h2>");
+            } else if (guess > targetNumber) {
+                out.println("<h2>Your guess is too high. Try again!</h2>");
+            } else {
+                out.println("<h2>Congratulations! You guessed the number!</h2>");
+                targetNumber = new Random().nextInt(100) + 1; // Reset game
+            }
+        } catch (NumberFormatException e) {
+            out.println("<h2>Invalid input. Please enter a valid number.</h2>");
+        }
+        out.println("<a href='guess'>Play Again</a>");
     }
 }
